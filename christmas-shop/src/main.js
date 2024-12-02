30548c54e156c323c9af05a1b371114cb6f2bc5b
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-12-02 10:58:02 +0300

    feat: add timer feature

--- christmas-shop/src/main.js
+++ christmas-shop/src/main.js
@@ -1,1 +1,9 @@
 import "./scss/main.scss";
+import { startCTATimer } from "./utils/timer";
+
+if (
+	window.location.pathname === "/" ||
+	window.location.pathname === "/index.html"
+) {
+	startCTATimer();
+}
